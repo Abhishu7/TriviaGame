67c57ce6f1045df952553e4e99a10920b19b4458
Abhishu Mukherjee <abhishu.m@utexas.edu>
2019-09-16 10:51:07 -0500

    created trivia questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,34 @@
+var triviaquestions = [{
+    question: "Where in the world would you find Chelsea FC",
+    answerList: [" Paris", " London", " Berlin", " Amsterdam"],
+    correctAnswer: " London",
+}, {
+    question: "What team does Cristiano Ronaldo play for?",
+    answerList: [" FC Barcelona", " Manchester United", " Real Madrid", " Juventus"],
+    correctAnswer: " Juventus",
+}, {
+    question: "Which player is notorious for the 'Hand of God' goal?",
+    answerList: [" Diego Maradona", " Pele", " Lionel Messi", " Ronaldinho"],
+    correctAnswer: " Diego Maradon",
+}, {
+    question: "Which league is the highest professional level of futbol in France?",
+    answerList: [" Bundesliga", " La Liga", " Ligue 1", " Liga Nos"],
+    correctAnswer: " Ligue 1",
+}, {
+    question: "Which country has never won the world cup?",
+    answerList: [" Netherlands", " Germany", " Uruguay", " England"],
+    correctAnswer: " Netherlands",
+}, {
+    question: "Which country has won the most FIFA World Cups?",
+    answerList: [" Argentina", " Brazil", " France", " Germany"],
+    correctAnswer: " Brazil",
+}, {
+    question: "How many teams participate in the FIFA World Cup?",
+    answerList: [" 16", " 48", " 1", " 32"],
+    correctAnswer: " 32"
+}, {
+    question: "Where will the next FIFA World Cup be held?",
+    answerList: [" USA/Canda/Mexico", " Russia", " Qatar", " Japan"],
+    correctAnswer: " Qatar",
+}
+]
